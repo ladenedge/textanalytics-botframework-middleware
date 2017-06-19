eda2e85998c8db71ab4541d5dc7e39f09e22ed2d
jadler1 <jacksonadler2020@u.northwestern.edu>
2017-06-19 12:12:12 -0400

    Basic functionality implemented

--- middleware.js
+++ middleware.js
@@ -1,7 +1,48 @@
 var TextAnalytics = require('textanalytics')
 
+/**
+ * Module for the Text analytics that creates a Bot Framework-friendly interface
+ */
 
-module.export = function (config) {
 
-	
+/**
+ * Serves as a middleware between the textanalytics API and the Bot Framework
+ * @param {Object} config Configuration for the module
+ * @param {string} config.endpoint Full endpoint for the Text Analytics API
+ * @param {string} config.apikey Full api key for the Text Analytics API
+ */
+var textanalytics_botframework_middleware = function (config) {
+	var textanalytics = new TextAnalytics(config);
+	var receive = function (event, next) {
+		if (!event || event === null) {
+			next();
+		}
+		else if (!event.message || event.message === null) {
+			next();
+		}
+		else if (!event.message.text || event.message.text === null) {
+			next();
+		}
+		else if (event.message.text.trim() === '') {
+			next();
+		}
+		else {
+			textanalytics.analyze(event.message.text, 
+				function (error,resp) {
+					if(error){
+						console.log(error);
+					}
+					else{						
+						var response_summary = `Sentiment score: ${resp[0][0].score}, /n Detected language: ${resp[1][0].detectedLanguages}, /n Key phrases: ${resp[2][0].keyPhrases}`;
+						console.log(response_summary);
+						next();
+					}
+						
+				}
+			)};
+
+	}
+	return receive;
 }
+
+module.export = textanalytics_botframework_middleware;
